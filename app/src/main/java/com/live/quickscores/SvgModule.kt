f71a38251ea2156592b91d872f032c002d132f91
otsimi <brightonobadia058@gmail.com>
2024-12-22 09:59:05 +0300

    onResume fragment lifecycle

--- app/src/main/java/com/live/quickscores/SvgModule.kt
+++ app/src/main/java/com/live/quickscores/SvgModule.kt
@@ -41,7 +41,7 @@ class SvgDecoder : ResourceDecoder<InputStream, SVG> {
 class SvgDrawableTranscoder : ResourceTranscoder<SVG?, PictureDrawable> {
     override fun transcode(
         toTranscode: Resource<SVG?>, options: Options
-    ): Resource<PictureDrawable>? {
+    ): Resource<PictureDrawable> {
         val svg = toTranscode.get()
         val picture = svg.renderToPicture()
         val drawable = PictureDrawable(picture)
